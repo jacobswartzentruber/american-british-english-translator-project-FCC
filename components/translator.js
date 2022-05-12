3204168c2cca8a96a4b2495d42d06fde0baaad79
jacob.swartzentruber <jsswart@gmail.com>
2022-05-12 10:10:14 -0500

    Implemented time translation

--- components/translator.js
+++ components/translator.js
@@ -17,19 +17,25 @@ class Translator {
       //convert american titles
       translated = this.checkMatches(translated, Object.keys(americanToBritishTitles), Object.values(americanToBritishTitles));
 
+      //convert american times
+      translated = this.checkTime(translated, locale);
+
     }else if(locale === 'british-to-american'){
       //convert british only words
       translated = this.checkMatches(translated, Object.keys(britishOnly), Object.values(britishOnly));
 
-      //convert american spelling
+      //convert british spelling
       translated = this.checkMatches(translated, Object.values(americanToBritishSpelling), Object.keys(americanToBritishSpelling));
 
-      //convert american titles
+      //convert british titles
       translated = this.checkMatches(translated, Object.values(americanToBritishTitles), Object.keys(americanToBritishTitles));
+
+      //convert british times
+      translated = this.checkTime(translated, locale);
     }
 
     if(translated === text) return 'Everything looks good to me!';
-    
+
     return translated;
   }
 
@@ -52,69 +58,24 @@ class Translator {
     return translated;
   }
 
-  /*translate (text, locale){
-    if(locale === 'american-to-british') console.log("convert to british");
-    if(locale === 'british-to-american') console.log("convert to american");
+  checkTime(sentence, locale){
+    let translated = sentence;
+    let punctuation = locale === 'american-to-british' ? ':' : '.';
+    let replacePunctuation = locale === 'american-to-british' ? '.' : ':';
 
-    let translated = text.split(/\s/).map(sequence => {
-        return this.translateSequence(sequence, locale);
+    let regex = new RegExp('(2[0-3]|[0-1]?[\\d])['+punctuation+'][0-5][\\d]', 'g');
+
+    let matches = translated.match(regex);
+
+    if(matches){
+      matches.forEach(match => {
+        let replacement = match.replace(punctuation, replacePunctuation);
+        translated = translated.replace(match, this.wrapTranslation(replacement));
       });
-
-    return translated.join(" ");
-  };
-
-  translateSequence (sequence, locale) {
-    let translated = this.translateTitle(sequence, locale);
-    if(sequence !== translated) return translated;
-
-    translated = sequence.split(/\b/).map(word => {
-      let lowercase = word.toLowerCase();
-
-      if(locale === 'american-to-british'){
-        //convert from american to british
-        if(Object.keys(americanOnly).includes(lowercase)) return this.formatTranslation(word, americanOnly[lowercase]);
-        if(Object.keys(americanToBritishSpelling).includes(lowercase)) return this.formatTranslation(word, americanToBritishSpelling[lowercase]);
-
-      }else if(locale === 'british-to-american'){
-        console.log(word);
-        //convert from british to american
-        if(Object.keys(britishOnly).includes(lowercase)) return this.formatTranslation(word, britishOnly[lowercase]);
-
-        let spellingFound = Object.keys(americanToBritishSpelling).find(key => americanToBritishSpelling[key] === lowercase);
-        if(spellingFound) return this.formatTranslation(word, spellingFound);
-      }
-
-      return word;
-    });
-
-    return translated.join("");
-  }
-  
-  translateTitle (sequence, locale) {
-    let lowercase = sequence.toLowerCase();
-
-    if(locale === 'american-to-british'){
-
-      if(Object.keys(americanToBritishTitles).includes(lowercase)) return this.formatTranslation(sequence, americanToBritishTitles[lowercase]);
-
-    }else if(locale === 'british-to-american'){
-
-      let titleFound = Object.keys(americanToBritishTitles).find(key => americanToBritishTitles[key] === lowercase);
-      if(titleFound) return this.formatTranslation(sequence, titleFound);
-      
     }
 
-    return sequence;
-  }*/
-
-  formatTranslation (original, translated) {
-    return this.wrapTranslation(this.caseFormat(original, translated));
-  }
-
-  caseFormat (original, translated){
-    if(/[A-Z]/.test(original[0])) return translated[0].toUpperCase() + translated.slice(1);
     return translated;
-  };
+  }
 
   wrapTranslation (word) {
     return '<span class="highlight">' + word + '</span>';
